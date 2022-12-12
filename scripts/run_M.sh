d7b6693eb34fd3b336d4bccb9e63615aaff557e4
SerdarHELLI <62758782+SerdarHelli@users.noreply.github.com>
2022-12-12 15:01:06 +0300

    SCRIPTS

--- scripts/run_M.sh
+++ scripts/run_M.sh
@@ -1,19 +1,33 @@
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
-#cd ..
 
-for model in FEDformer Autoformer Informer Transformer
-do
-
-result_path="/content/drive/MyDrive/Serdar/CDR_Results/$result_path _Mul"
-
-# ETT m1
 python -u run.py \
-  --is_training 1 --model=$model  --task_id=Internet --data=custom --root_path="/content/" --data_path="df.csv" --features=M \
-              --c_out=4 --enc_in=4 --dec_in=4 --seq_len=36 --label_len=18 --pred_len=36 \
-              --itr=1  --e_layers=2 --d_layers=1   --factor=3 --batch_size=8 --result_path=$result_path\
+--is_training 1 --model=FEDformer --version=Fourier --task_id=Internet --data=custom --root_path="/content/" --data_path="df.csv" --features=M \
+            --c_out=4 --enc_in=4 --dec_in=4 --seq_len=36 --label_len=18 --pred_len=36 \
+            --itr=1  --e_layers=2 --d_layers=1   --factor=3 --batch_size=8 \
+            --result_path="/content/drive/MyDrive/Serdar/CDR_Results/FEDFormerFourier_Mul"\
 
 
+python -u run.py \
+--is_training 1 --model=FEDformer --version=Wavelets --task_id=Internet --data=custom --root_path="/content/" --data_path="df.csv" --features=M \
+            --c_out=4 --enc_in=4 --dec_in=4 --seq_len=36 --label_len=18 --pred_len=36 \
+            --itr=1  --e_layers=2 --d_layers=1   --factor=3 --batch_size=8 \
+            --result_path="/content/drive/MyDrive/Serdar/CDR_Results/FEDFormerWavelet_Mul"\
+python -u run.py \
+--is_training 1 --model=Autoformer --version=Fourier --task_id=Internet --data=custom --root_path="/content/" --data_path="df.csv" --features=M \
+            --c_out=4 --enc_in=4 --dec_in=4 --seq_len=36 --label_len=18 --pred_len=36 \
+            --itr=1  --e_layers=2 --d_layers=1   --factor=3 --batch_size=8 \
+            --result_path="/content/drive/MyDrive/Serdar/CDR_Results/Autoformer_Mul"\
+    
+python -u run.py \
+--is_training 1 --model=Informer --version=Fourier --task_id=Internet --data=custom --root_path="/content/" --data_path="df.csv" --features=M \
+            --c_out=4 --enc_in=4 --dec_in=4 --seq_len=36 --label_len=18 --pred_len=36 \
+            --itr=1  --e_layers=2 --d_layers=1   --factor=3 --batch_size=8 \
+            --result_path="/content/drive/MyDrive/Serdar/CDR_Results/Informer_Mul"\
 
-done
+python -u run.py \
+--is_training 1 --model=Transformer --version=Fourier --task_id=Internet --data=custom --root_path="/content/" --data_path="df.csv" --features=M \
+            --c_out=4 --enc_in=4 --dec_in=4 --seq_len=36 --label_len=18 --pred_len=36 \
+            --itr=1  --e_layers=2 --d_layers=1   --factor=3 --batch_size=8 \
+            --result_path="/content/drive/MyDrive/Serdar/CDR_Results/Transformer_Mul"\
 
--- scripts/run_S.sh
+++ scripts/run_S.sh
@@ -1,15 +1,33 @@
-export CUDA_VISIBLE_DEVICES=1
-
-#cd ..
-
-for model in FEDformer Autoformer Informer Transformer
-do
-
-result_path="/content/drive/MyDrive/Serdar/CDR_Results/$result_path _Mul"
-
-echo $result_path
+export CUDA_VISIBLE_DEVICES=0
 
 
+python -u run.py \
+--is_training 1 --model=FEDformer --version=Fourier --task_id=Internet --data=custom --root_path="/content/" --data_path="df.csv" --features=S \
+            --c_out=1--enc_in=1 --dec_in=1 --seq_len=36 --label_len=18 --pred_len=36 \
+            --itr=1  --e_layers=2 --d_layers=1   --factor=3 --batch_size=8 \
+            --result_path="/content/drive/MyDrive/Serdar/CDR_Results/FEDFormerFourier_Uni"\
 
-done
+
+python -u run.py \
+--is_training 1 --model=FEDformer --version=Wavelets --task_id=Internet --data=custom --root_path="/content/" --data_path="df.csv" --features=S \
+            --c_out=1 --enc_in=1 --dec_in=1 --seq_len=36 --label_len=18 --pred_len=36 \
+            --itr=1  --e_layers=2 --d_layers=1   --factor=3 --batch_size=8 \
+            --result_path="/content/drive/MyDrive/Serdar/CDR_Results/FEDFormerWavelet_Uni"\
+python -u run.py \
+--is_training 1 --model=Autoformer --version=Fourier --task_id=Internet --data=custom --root_path="/content/" --data_path="df.csv" --features=S \
+            --c_out=1 --enc_in=1 --dec_in=1 --seq_len=36 --label_len=18 --pred_len=36 \
+            --itr=1  --e_layers=2 --d_layers=1   --factor=3 --batch_size=8 \
+            --result_path="/content/drive/MyDrive/Serdar/CDR_Results/Autoformer_Uni"\
+    
+python -u run.py \
+--is_training 1 --model=Informer --version=Fourier --task_id=Internet --data=custom --root_path="/content/" --data_path="df.csv" --features=S \
+            --c_out=1 --enc_in=1 --dec_in=1 --seq_len=36 --label_len=18 --pred_len=36 \
+            --itr=1  --e_layers=2 --d_layers=1   --factor=3 --batch_size=8 \
+            --result_path="/content/drive/MyDrive/Serdar/CDR_Results/Informer_Uni"\
+
+python -u run.py \
+--is_training 1 --model=Transformer --version=Fourier --task_id=Internet --data=custom --root_path="/content/" --data_path="df.csv" --features=S \
+            --c_out=1 --enc_in=1 --dec_in=1 --seq_len=36 --label_len=18 --pred_len=36 \
+            --itr=1  --e_layers=2 --d_layers=1   --factor=3 --batch_size=8 \
+            --result_path="/content/drive/MyDrive/Serdar/CDR_Results/Transformer_Uni"\
 
