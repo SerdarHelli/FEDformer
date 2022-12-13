cf5d0e3ab30c1624517a626cda1126e1ee401e1a
SerdarHELLI <62758782+SerdarHelli@users.noreply.github.com>
2022-12-13 10:21:37 +0300

    Update run_S.sh

--- scripts/run_S.sh
+++ scripts/run_S.sh
@@ -6,7 +6,7 @@ echo 'FEDFORMER Fourier'
 
 python -u run.py \
 --is_training 1 --model=FEDformer --version=Fourier --task_id=Internet --data=custom --root_path="/content/" --data_path="df.csv" --features=S \
-            --c_out=1--enc_in=1 --dec_in=1 --seq_len=36 --label_len=18 --pred_len=36 \
+            --c_out=1 --enc_in=1 --dec_in=1 --seq_len=36 --label_len=18 --pred_len=36 \
             --itr=1  --e_layers=2 --d_layers=1   --factor=3 --batch_size=8 \
             --result_path="/content/drive/MyDrive/Serdar/CDR_Results/FEDFormerFourier_Uni"\
 
